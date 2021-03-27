7265f40a6567e57fb59d0eb9930d244f856eebf6
Bino <hoaianh.bino@gmail.com>
2021-03-27 06:17:13 +0200

    Add products listing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,39 @@
 import Head from 'next/head';
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className='min-h-screen flex flex-col items-center justify-center'>
       <Head>
-        <title>Create Next App</title>
+        <title>Next Store</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='container mx-auto'>
-        <h1 className='text-4xl text-bold text-green-700 text-center'>
-          Welcome to Next Store
+      <main className='container mx-auto my-6 px-4'>
+        <h1 className='text-4xl text-bold mb-6'>
+          All Products
         </h1>
+        <ol className='grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-6 lg:grid-cols-4 '>
+          {products.map(({ id, name, price, images: [image] }) => {
+            return (
+              <li key={id}>
+                <img src={image.src} alt="" className='w-full rounded' />
+                <h2 className='mt-3'>{name}</h2>
+                <div className='font-bold'>${price}</div>
+              </li>
+            );
+          })}
+        </ol>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const products = require('../__fixtures__/products.json')
+
+  return {
+    props: {
+      products
+    }
+  }
 }
