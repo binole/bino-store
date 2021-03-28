09e9e4542a9defaa6e32ecd756abea97aabdd1f9
Bino <hoaianh.bino@gmail.com>
2021-03-28 08:04:59 +0300

    Add links to product detail

---
+++ pages/products/index.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Categories from '../../components/categories';
+import Container from '../../components/container';
+import Layout from '../../components/layout';
+import ProductItem from '../../components/product-item';
+import ProductList from '../../components/product-list';
+import apiClient from '../../utils/api-client';
+
+export async function getStaticProps() {
+  const products = await apiClient('products');
+  const categories = await apiClient('categories');
+
+  return {
+    props: {
+      categories,
+      products
+    }
+  }
+}
+
+export default function Products({ products, categories }) {
+  const { query: { category = [] } } = useRouter();
+
+  return (
+    <Layout pageTitle='Products'>
+      <Container className='my-6'>
+        <div className="grid grid-cols-12 gap-4 md:gap-6">
+          <div className="col col-span-12 sm:col-span-3">
+            <aside>
+              <Categories categories={categories} activeCategories={category} />
+            </aside>
+          </div>
+          <div className="col col-span-12 sm:col-span-9">
+            <ProductList
+              items={products}
+              activeCategories={category}
+              renderItem={({ name, price, slug, images: [image] }) => {
+                return (
+                  <Link href={`/products/${slug}`}>
+                    <a>
+                      <ProductItem name={name} price={price} image={image.src} />
+                    </a>
+                  </Link>
+                );
+              }}
+            />
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
+
